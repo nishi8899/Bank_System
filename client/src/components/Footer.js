c805cce9b5aa8e8ff8d908500d67f92eda07907c
nishi8899 <66168880+nishi8899@users.noreply.github.com>
2021-05-13 19:30:39 +0530

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,18 +9,9 @@ const Footer = () => {
   }
   return (
     <footer>
-      <div>
-        <h4>Contact us</h4>
-        <p>Telephone: 123-123-1234</p>
-        <p>Email: sparkbank@email.com</p>
-      </div>
+      
       <p className='copyright'>Spark Bank Copyright &copy; {getYr()}</p>
-      <div>
-        <h4>Address</h4>
-        <p>89,Malviya Nagar,</p>
-        <p>Near Jagatpura,</p>
-        <p>Jaipur, India</p>
-      </div>
+      
     </footer>
   )
 }
