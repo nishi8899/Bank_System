212a75b58c2eba12720b5370b4814bcc7272983f
nishi8899 <66168880+nishi8899@users.noreply.github.com>
2021-05-14 21:48:14 +0530

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,6 +11,15 @@ const Footer = () => {
     <footer>
       
       <p  className='copyright'>Spark Bank Copyright &copy; {getYr()}</p>
+ <div class="col-md-6 text-center">
+            <ul class="social-icons">
+              Contact Me:
+              <li><a target="_blank" class="github" href="https://github.com/nishi8899/"><i class="fab fa-github fa-2x"></i></a></li>
+              
+              <li><a target="_blank" class="linkedin" href="https://www.linkedin.com/in/nishi-singh-722855175"><i class="fab fa-linkedin fa-2x"></i></a></li>   
+            </ul>
+        </div>
+      </div>
       
     </footer>
   )
